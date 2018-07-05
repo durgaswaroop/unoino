ed46e663feed413e7f411b66a657e3472c9a9dbc
DurgaSwaroop <durgaswaroop@gmail.com>
2018-07-06 02:23:08 +0530

    Make game test method names more descriptive

--- tests/test_game.py
+++ tests/test_game.py
@@ -4,14 +4,13 @@ from uno.Game import Game
 
 class TestGame(unittest.TestCase):
 
-    def test_invalid_players(self):
-        # Game should raise value error when players <= 0
+    # Game should raise value error when players <= 0
+    def test_should_raise_value_error_for_invalid_player_count(self):
         self.assertRaises(ValueError, Game, 0)
         self.assertRaises(ValueError, Game, -2)
         self.assertRaises(ValueError, Game, -100)
 
+    # If Game has non-zero players, game should have non-zero players
     def test_player_numbers(self):
-        # If Game has non-zero players, game should have non-zero players
         self.assertEqual(10, Game(players=10).players)
         self.assertEqual(100, Game(players=100).players)
-    
