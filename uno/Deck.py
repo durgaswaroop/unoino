f7318119433f7ba271d8236400e88c2700c91f5f
DurgaSwaroop <durgaswaroop@gmail.com>
2018-07-07 03:21:58 +0530

    Update Dock class to generate all combinations of colors, numbers and action cards
    With these we've 100 cards generated. Wild cards are left now.

--- uno/Deck.py
+++ uno/Deck.py
@@ -1,4 +1,5 @@
 from uno.Card import Card
+import itertools
 
 FULL_DECK_SIZE = 108
 
@@ -15,43 +16,19 @@ class Deck:
         colors = ["RED", "BLUE", "GREEN", "YELLOW"]  # No Pink Ranger :(
         numbers = list(range(10)) + list(range(1, 10))
         # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        actions = ["SKIP", None, None]
 
-        # cards = [Card(color) for color, number in zip(colors, numbers, actions)]
+        # Generate all combinations of colors and numbers
+        numbered_cards = [Card(col, num) for col, num in
+                          itertools.product(colors, numbers)]
 
-        # colored_cards = []
+        # Twice because each action is present twice in a color
+        actions = ["SKIP", "DRAW_TWO", "REVERSE"] * 2
 
-        # cards = [Card("") for i in range(FULL_DECK_SIZE)]
+        action_cards = [Card(col, action=action) for col, action in
+                        itertools.product(colors, actions)]
 
-        reds = [Card("RED") for _ in range(25)]
-        blues = [Card("BLUE") for _ in range(25)]
-        greens = [Card("GREEN") for _ in range(25)]
-        yellows = [Card("YELLOW") for _ in range(25)]
+        others= [Card("")] * 8
 
-        # Add numbers
-        for i, number in enumerate(numbers):
-            reds[i].number = number
-            blues[i].number = number
-            greens[i].number = number
-            yellows[i].number = number
-
-        # Add action cards
-        # There will be 6 action cards of each color
-        for i in range(len(reds) - 1, len(reds) - 7, -1):
-            reds[i].is_action_card = True
-            blues[i].is_action_card = True
-            greens[i].is_action_card = True
-            yellows[i].is_action_card = True
-
-        for colored_cards in [reds, blues, greens, yellows]:
-            colored_cards[19].action = "SKIP"
-            colored_cards[20].action = "SKIP"
-            colored_cards[21].action = "DRAW_TWO"
-            colored_cards[22].action = "DRAW_TWO"
-            colored_cards[23].action = "REVERSE"
-            colored_cards[24].action = "REVERSE"
-
-        others = [Card("")] * (FULL_DECK_SIZE - 25 * 4)
-        return reds + blues + greens + yellows + others
+        return numbered_cards + action_cards + others
 
 # cards = Deck().cards
