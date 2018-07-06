3893e531a44e8d5349ad12aab8fd7ad52ae458cd
DurgaSwaroop <durgaswaroop@gmail.com>
2018-07-07 03:18:58 +0530

    Add optional number and action parameters for Card constructor
    If action is not None, we'll set the is_action_card flag.

--- uno/Card.py
+++ uno/Card.py
@@ -1,12 +1,15 @@
 class Card:
     color = None
-    number = None  # Start with -100 as the default is already 0
-    is_action_card = None
+    number = None
+    is_action_card = False
     action = None
 
-    def __init__(self, color):
+    def __init__(self, color, number=None, action=None):
         self.color = color
-        # self.number = number
+        self.number = number
+        if action:  # If an action has been passed, set the action flag
+            self.action = action
+            self.is_action_card = True
 
     def __repr__(self):
         return f"Card({self.color}, {self.number}, {self.is_action_card})"
