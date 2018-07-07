246abc39a56836330a7b23b5e15e2abb3f4c42ed
DurgaSwaroop <durgaswaroop@gmail.com>
2018-07-07 20:52:12 +0530

    Add num_players parameter in Game class
    Instead of doing len(players), everytime, it is better to use this varaiable in all the methods where we need the number of players.

--- uno/Game.py
+++ uno/Game.py
@@ -8,6 +8,7 @@ class Game:
     players = None
     cards = None
     shuffled_deck = None
+    num_players = None
 
     # Game starts with a non-zero set of players and
     def __init__(self, players, deck):
@@ -20,6 +21,7 @@ class Game:
             raise ValueError("Can't start a game without a deck of cards")
 
         self.players = players
+        self.num_players = len(players)
         self.cards = deck
 
     # Start the game
@@ -38,13 +40,11 @@ class Game:
         return random.sample(self.cards, len(self.cards))
 
     def distribute_cards(self):
-        num_players = len(self.players)
         cards_to_distribute = \
-            self.shuffled_deck[:(num_players * CARDS_PER_PLAYER)]
+            self.shuffled_deck[:(self.num_players * CARDS_PER_PLAYER)]
 
         for i, player in enumerate(self.players):
-            player.cards = cards_to_distribute[i:: num_players]
-
+            player.cards = cards_to_distribute[i:: self.num_players]
 
 # from uno.Player import Player
 # from uno.Deck import Deck
