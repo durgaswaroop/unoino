fe4cc6e64ec578d9401d79da35f0b2e18c242282
DurgaSwaroop <durgaswaroop@gmail.com>
2018-07-07 06:46:15 +0530

    Game should raise error for both empty player list and empty deck

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,18 +1,21 @@
 import unittest
+
+from uno.Deck import Deck
 from uno.Game import Game
 
+empty_deck = []
+deck = Deck().cards
+empty_players = []
 
 class TestGame(unittest.TestCase):
 
-    # Game should raise value error when players <= 0
-    def test_should_raise_value_error_for_invalid_player_count(self):
-        self.assertRaises(ValueError, Game, 0)
-        self.assertRaises(ValueError, Game, -2)
-        self.assertRaises(ValueError, Game, -100)
+    # Game should raise value error when players = 0
+    def test_should_raise_value_error_for_zero_players(self):
+        self.assertRaises(ValueError, Game, [], deck)
 
     # Game can't start without a deck. Expect ValueError
     def test_game_raises_error_for_no_deck(self):
-        self.assertRaises(ValueError, Game, 10, None)
+        self.assertRaises(ValueError, Game, [1], None)
 
     # Game can't start with an empty deck. Expect ValueError
     def test_game_raises_error_on_empy_deck(self):
--- uno/Game.py
+++ uno/Game.py
@@ -1,15 +1,15 @@
+# A Game needs Players and a Deck of cards
 class Game:
     players = None
     cards = None
 
     # Game starts with a non-zero set of players and
-    def __init__(self, players, deck=None):
-        if players <= 0:
-            raise ValueError(
-                f"Can't have a game with {players} Players. Go home.")
+    def __init__(self, players, deck):
+        if not players:
+            raise ValueError("Can't have a game with no Players. Go home.")
 
         if not deck:
-            raise ValueError(f"Can't start a game without a deck of cards")
+            raise ValueError("Can't start a game without a deck of cards")
 
         self.players = players
         self.cards = deck
